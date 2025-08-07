b4a4804c34b21a878cca9d0d777c409fb0bd2b3b
JoseFR2001 <jose.rafael.mendoza.2001@gmail.com>
2025-08-07 10:58:53 -0300

    Controladores de user sin validaciones creado

---
+++ src/controllers/task.controller.js
@@ -0,0 +1,41 @@
+import Task from "../models/task.js";
+
+export const createTask = async(req,res) =>{
+    try {
+        
+    } catch (error) {
+        res.error(500).json({error : error.message})
+    }
+}
+
+export const getTask = async(req,res) =>{
+    try {
+        
+    } catch (error) {
+        res.error(500).json({error : error.message})
+    }
+}
+
+export const getAllTask = async(req,res) =>{
+    try {
+        
+    } catch (error) {
+        res.error(500).json({error : error.message})
+    }
+}
+
+export const updateTask = async(req,res) =>{
+    try {
+        
+    } catch (error) {
+        res.error(500).json({error : error.message})
+    }
+}
+
+export const deleteTask = async(req,res) =>{
+    try {
+        
+    } catch (error) {
+        res.error(500).json({error : error.message})
+    }
+}
--- src/models/task.model.js
+++ src/models/task.model.js
@@ -1,17 +1,10 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../config/database.js";
 
-const task = sequelize.define("task", {
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    unique: true,
-    autoIncrement: true,
-    primaryKey: true,
-  },
+const Task = sequelize.define("task", {
   title: { type: DataTypes.STRING(100), unique: true, allowNull: false },
   description: { type: DataTypes.STRING(100), allowNull: false },
   isComplete: { type: DataTypes.BOOLEAN, defaultValue: false },
 });
 
-export default task
+export default Task
