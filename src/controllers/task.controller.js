144c3f7dbe573f3a9549ba04ea1691a611d25055
[JoseFR2001] <[jose.rafael.mendoza.2001@gmail.com]>
2025-08-08 00:51:53 -0300

    Las rutas esan terminadas y el servidor se levanta correctamente

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,41 +1,58 @@
-import Task from "../models/task.js";
+import Task from "../models/task.model.js";
 
-export const createTask = async(req,res) =>{
-    try {
-        
-    } catch (error) {
-        res.error(500).json({error : error.message})
-    }
-}
+export const createTask = async (req, res) => {
+  try {
+    const crearTarea = await Task.create(req.body);
+    return res.status(201).json(crearTarea);
+  } catch (error) {
+    res.error(500).json({ error: error.message });
+  }
+};
 
-export const getTask = async(req,res) =>{
-    try {
-        
-    } catch (error) {
-        res.error(500).json({error : error.message})
-    }
-}
+export const getByIdTask = async (req, res) => {
+  try {
+    const task = await task.findByPk(req.params.id);
+    if (!Task) return res.status(404).json({ message: "El tarea no existe" });
+    return res.status(200).json(Task);
+  } catch (error) {
+    res.error(500).json({ error: error.message });
+  }
+};
 
-export const getAllTask = async(req,res) =>{
-    try {
-        
-    } catch (error) {
-        res.error(500).json({error : error.message})
-    }
-}
+export const getAllTask = async (req, res) => {
+  try {
+    const tasks = await Task.findAll();
+    return res.status(200).json(tasks);
+  } catch (error) {
+    res.error(500).json({ error: error.message });
+  }
+};
 
-export const updateTask = async(req,res) =>{
-    try {
-        
-    } catch (error) {
-        res.error(500).json({error : error.message})
+export const updateTask = async (req, res) => {
+  try {
+    const [update] = await Task.update(req.body, {
+      where: { id: req.params.id },
+    });
+    if (update) {
+      const actualizarTask = await Task.findByPk(req.params.id);
+      return res.status(200).json(actualizarTask);
+    } else {
+      return res.status(404).json({ message: "Tarea no encontrada" });
     }
-}
+  } catch (error) {
+    res.error(500).json({ error: error.message });
+  }
+};
 
-export const deleteTask = async(req,res) =>{
-    try {
-        
-    } catch (error) {
-        res.error(500).json({error : error.message})
-    }
-}
+export const deleteTask = async (req, res) => {
+  try {
+    const eliminarTarea = await Task.destroy({
+      where: { id: req.params.id },
+    });
+    if (!eliminarTarea)
+      return res.status(404).json({ message: "El tarea no existe" });
+    return res.status(200).json({ message: "Tarea eliminada" });
+  } catch (error) {
+    res.error(500).json({ error: error.message });
+  }
+};
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,54 +1,58 @@
-import { where } from "sequelize";
 import User from "../models/user.model.js";
 
-export const createUser = async(req,res) =>{
-    try {
-        const crearUsusario = await User.create(req.body)
-        return res.status(201).json(crearUsusario)
-    } catch (error) {
-        res.error(500).json({error : error.message})
-    }
-}
+export const createUser = async (req, res) => {
+  try {
+    const crearUsusario = await User.create(req.body);
+    return res.status(201).json(crearUsusario);
+  } catch (error) {
+    res.error(500).json({ error: error.message });
+  }
+};
 
-export const getUser = async(req,res) =>{
-    try {
-        const user = await User.findByPk(req.params.id)
-        if(!user) return res.status(404).json({message : "El usuario no existe"})
-        return res.status(200).json(user)
-    } catch (error) {
-        res.error(500).json({error : error.message})
-    }
-}
+export const getByIdUser = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (!user) return res.status(404).json({ message: "El usuario no existe" });
+    return res.status(200).json(user);
+  } catch (error) {
+    res.error(500).json({ error: error.message });
+  }
+};
 
-export const getAllUser = async(req,res) =>{
-    try {
-        const users = await User.findAll()
-        return res.status(200).json(users)
-    } catch (error) {
-        res.error(500).json({error : error.message})
-    }
-}
+export const getAllUser = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    return res.status(200).json(users);
+  } catch (error) {
+    res.error(500).json({ error: error.message });
+  }
+};
 
-export const updateUser = async(req,res) =>{
-    try {
-        const [update] = await User.update(req.body, { where : {id : req.params.id}})
-        if (update) {
-            const actualizarUser = await User.findByPk(req.params.id)
-            return res.status(200).json(actualizarUser)
-        } else {
-            return res.status(404).json({ message : "Usuario no encontrado"})
-        }
-    } catch (error) {
-        res.error(500).json({error : error.message})
+export const updateUser = async (req, res) => {
+  try {
+    const [update] = await User.update(req.body, {
+      where: { id: req.params.id },
+    });
+    if (update) {
+      const actualizarUser = await User.findByPk(req.params.id);
+      return res.status(200).json(actualizarUser);
+    } else {
+      return res.status(404).json({ message: "Usuario no encontrado" });
     }
-}
+  } catch (error) {
+    res.error(500).json({ error: error.message });
+  }
+};
 
-export const deleteUser = async(req,res) =>{
-    try {
-        const eliminarUsusario = await User.destroy({ where : {id : req.params.id}})
-        if(!eliminarUsusario) return res.status(404).json({ message : "El usuario no existe"})
-        return res.status(200).json({ message : "Usuario eliminado"})
-    } catch (error) {
-        res.error(500).json({error : error.message})
-    }
-}
+export const deleteUser = async (req, res) => {
+  try {
+    const eliminarUsusario = await User.destroy({
+      where: { id: req.params.id },
+    });
+    if (!eliminarUsusario)
+      return res.status(404).json({ message: "El usuario no existe" });
+    return res.status(200).json({ message: "Usuario eliminado" });
+  } catch (error) {
+    res.error(500).json({ error: error.message });
+  }
+};
