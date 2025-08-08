27f7a4653f2e6a17bcf4502c0055127ee622f73d
[JoseFR2001] <[jose.rafael.mendoza.2001@gmail.com]>
2025-08-08 01:18:33 -0300

    Validaciones de User listo

--- src/config/database.js
+++ src/config/database.js
@@ -4,20 +4,21 @@ import dotevn from "dotenv";
 dotevn.config();
 
 export const sequelize = new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PASSWORD, 
-    {
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-});
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
 
-export const initDB = async () =>{
-    try {
-  await sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-  await sequelize.sync()
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
-}
+export const initDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+    await sequelize.sync();
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,38 @@ import User from "../models/user.model.js";
 
 export const createUser = async (req, res) => {
   try {
+    const { name, email, password } = req.body;
+    if (
+      name === "" ||
+      name === undefined ||
+      email === "" ||
+      email === undefined ||
+      password === "" ||
+      password === undefined
+    )
+      return res
+        .status(400)
+        .json({ message: "Los campos no deben estar vacios" });
+
+    if (name.length > 100)
+      return res
+        .status(400)
+        .json({ message: "El nombre no debe tener más de 100 caracteres" });
+
+    if (email.length > 100)
+      return res
+        .status(400)
+        .json({ message: "El email no debe tener más de 100 caracteres" });
+
+    const emailExiste = await User.findOne({ where: { email: email } });
+    if (emailExiste)
+      return res.status(400).json({ message: "Ya existe el email" });
+
+    if (password.length > 100)
+      return res
+        .status(400)
+        .json({ message: "El password no debe tener más de 100 caracteres" });
+
     const crearUsusario = await User.create(req.body);
     return res.status(201).json(crearUsusario);
   } catch (error) {
@@ -30,6 +62,37 @@ export const getAllUser = async (req, res) => {
 
 export const updateUser = async (req, res) => {
   try {
+    const { name, email, password } = req.body;
+    if (
+      name === "" ||
+      name === undefined ||
+      email === "" ||
+      email === undefined ||
+      password === "" ||
+      password === undefined
+    )
+      return res
+        .status(400)
+        .json({ message: "Los campos no deben estar vacios" });
+
+    if (name.length > 100)
+      return res
+        .status(400)
+        .json({ message: "El nombre no debe tener más de 100 caracteres" });
+
+    if (email.length > 100)
+      return res
+        .status(400)
+        .json({ message: "El email no debe tener más de 100 caracteres" });
+
+    const emailExiste = await User.findOne({ where: { email: email } });
+    if (emailExiste)
+      return res.status(400).json({ message: "Ya existe el email" });
+
+    if (password.length > 100)
+      return res
+        .status(400)
+        .json({ message: "El password no debe tener más de 100 caracteres" });
     const [update] = await User.update(req.body, {
       where: { id: req.params.id },
     });
